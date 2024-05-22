09297d57a9eb93cc1e73c8d22715e5774c0a5833
dilovar1992 <dellovar56@gmail.com>
2024-05-22 09:11:40 -0400

    solved Sorting1DArray

--- src/dilovar/Sorting1DArray.java
+++ src/dilovar/Sorting1DArray.java
@@ -8,20 +8,19 @@ public class Sorting1DArray {
     //sort an array without using a special method
     public static void main(String[] args) {
         Integer[] num={2,6,5,9,3,12,15,17};
-        Integer[] result=new Integer[num.length];
-        int min=100;
-        List<Integer> number=new ArrayList<>(Arrays.asList(num));
-        for (int i = 0; i < number.size(); i++) {
-            if (min> number.get(i)) {
-                min = number.get(i);
-                number.remove(min);
-            }
-            result[i]=min;
 
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num.length-1; j++) {
+                if (num[j]>num[j+1]){
+                    int temp=num[j];
+                    num[j]=num[j+1];
+                    num[j+1]=temp;
+                }
+            }
 
         }
-        System.out.println(min);
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(num));
+
     }
 
 
